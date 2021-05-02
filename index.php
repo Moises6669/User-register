dceca541eff7062022eea6f27575be4182c53c9a
Nobody <moisesalex132@gmail.com>
2021-05-01 22:10:22 -0600

    ➕ADD: Agregando el Home y rutas

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-
-require "databaseconnect.php";
-
-// database connection and tables 
-$querys = new Conexiones();
-
-$querys->connect();
-
-$table = "USUARIOS";
-
-//error and message handling
-$aErrores = array();
-$aMenssages = array();
-
-//form
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-if (!empty($_POST)) {
-
-    if (isset($name) && isset($email) && isset($password)) {
-
-        if (!empty($name) && !empty($email) && !empty($password)) {
-            $querys->NewUser($name, $email, $password);
-        }
-    }
-} else {
-    echo '<p>No se obtuvieron los datos</p>';
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,36 +5,19 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-    
-    <script src="./js/signup.js"> </script>
-    
-    <link rel="stylesheet" href="./styles/main.css">
-
-    <link rel="stylesheet" href="./styles/normalize.css">
-
-    <title>Formulario</title>
+    <link rel="stylesheet" href="./styles/home.css">
+    <title>Home</title>
 </head>
 
 <body>
-    <div  class="container">
-        <h2>Registrate</h2>
-        <form action="index.php" method="POST" id="signup" name="signup">
-
-            <input id="name" placeholder="nombre" type="text" name="name">
-
-            <input id="email" placeholder="email" type="text" name="email">
-
-            <input id="password" placeholder="contraseña" type="password" name="password">
-
-            <input id="aceptar" class="enviar" type="submit" value="ACEPTAR">
-
-        </form>
+    <div class="container">
+        <h1>AGENDA DE CONTACTO</h1>
     </div>
-
-
+    <div class="cont-buttons">
+         <button type="submit" name="signup" class="registro" onclick="location='signup.php'">Registrate</button>
+         <button type="submit" name="login" class="secion">Inicia secion</button>
+    </div>
 </body>
 
 </html>
+
---
+++ signup.php
@@ -0,0 +1,68 @@
+<?php
+
+require "databaseconnect.php";
+
+// database connection and tables 
+$querys = new Conexiones();
+
+$querys->connect();
+
+$table = "USUARIOS";
+
+//error and message handling
+$aErrores = array();
+$aMenssages = array();
+
+if (!empty($_POST)) {
+
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+
+        if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+            $querys->NewUser($_POST['name'], $_POST['email'],  $_POST['password']);
+        }
+    }
+} else {
+    // echo '<p>No se obtuvieron los datos</p>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+
+    <script src="./js/signup.js"> </script>
+
+    <link rel="stylesheet" href="./styles/signup.css">
+
+    <link rel="stylesheet" href="./styles/normalize.css">
+
+    <title>Formulario</title>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Registrate</h2>
+        <form action="signup.php" method="POST" id="signup" name="signup">
+
+            <input id="name" placeholder="nombre" type="text" name="name">
+
+            <input id="email" placeholder="email" type="text" name="email">
+
+            <input id="password" placeholder="contraseña" type="password" name="password">
+
+            <input id="aceptar" class="enviar" type="submit" value="ACEPTAR">
+
+        </form>
+    </div>
+
+
+</body>
+
+</html>
